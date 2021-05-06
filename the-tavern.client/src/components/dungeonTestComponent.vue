232f232c56f88c7cd7227606bd1ee376d087c5e5
Sam Limpic <samlimpic@gmail.com>
2021-05-06 11:47:51 -0600

    dungeon API test

--- the-tavern.client/src/components/dungeonTestComponent.vue
+++ the-tavern.client/src/components/dungeonTestComponent.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="dungeonTestComponent row justify-content-center">
+  <div class="dungeonTestComponent row justify-content-center" v-if="state.rawData">
+    {{ state.rawData }}
+    <div class="col-12"></div>
   </div>
 </template>
 
 <script>
 import { computed, onMounted, reactive } from 'vue'
 import { AppState } from '../AppState'
-// import Notification from '../utils/Notification'
+import { dungeonTestService } from '../services/DungeonTestService'
+import Notification from '../utils/Notification'
 
 export default {
   name: 'DungeonTestComponent',
   setup() {
     const state = reactive({
       loading: true,
-      newPost: computed(() => AppState.newPost),
-      bugs: computed(() => AppState.bugs)
+      character: computed(() => AppState.character),
+      rawData: computed(() => AppState.rawData)
     })
     onMounted(async() => {
-      // try {
-      //   await dungeonTestService.getCharacter()
-      // } catch (error) {
-      //   Notification.toast('Error: ' + error, 'error')
-      // }
+      try {
+        await dungeonTestService.getCharacter(state.character)
+      } catch (error) {
+        Notification.toast('Error: ' + error, 'error')
+      }
     })
     return {
       state
