23a343616e1fb64f8d69d455c24bb45815b40f4d
Kevin Powell <55499063+kpohiostyle@users.noreply.github.com>
2021-05-06 11:54:57 -0600

    backend phase 1 complete

--- the-tavern.server/server/models/Question.js
+++ the-tavern.server/server/models/Question.js
@@ -5,27 +5,15 @@ const Answer = new Schema({
   body: { type: String, required: true },
   value: { type: String, required: true }
 })
-Answer.virtual('creator', {
-  localField: 'creatorId',
-  ref: 'Account',
-  foreignField: '_id',
-  justOne: true
-})
+
 const Question = new Schema(
   {
-    id: { type: Number, required: true },
+    number: { type: Number, required: true },
     query: { type: String, required: true },
     answers: [Answer]
 
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
-
-Question.virtual('creator', {
-  localField: 'creatorId',
-  ref: 'Account',
-  foreignField: '_id',
-  justOne: true
-})
 
 export default Question
--- the-tavern.server/server/services/JobsService.js
+++ the-tavern.server/server/services/JobsService.js
@@ -1,14 +1,25 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
 class JobsService {
   async getJob(query) {
     const job = await dbContext.Jobs.findOne({ role: query.role, style: query.style })
+    if (!job) {
+      throw new BadRequest('Invalid Query')
+    }
     return job
   }
 
   async createJob(body) {
     return await dbContext.Jobs.create(body)
   }
-}
 
+  async editJob(body) {
+    const job = await dbContext.Jobs.findByIdAndUpdate({ _id: body.id }, body, { new: true })
+    if (!job) {
+      throw new BadRequest('Invalid request')
+    }
+    return job
+  }
+}
 export const jobsService = new JobsService()
