3dcee06f70438e73b0fd1a2719f7782405a805a8
Veronica Blake/Noyce <vrex149@gmail.com>
2021-05-13 17:10:04 -0600

    About page skeletons

---
+++ the-tavern.client/src/components/AboutComponents/RulesComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="shadow bg-light text-center m-3 p-md-5 p-4">
+    <div class="StartingComponent">
+      <h2><u>DnD Rules</u></h2>
+      <h3>
+        Here's where we have a rundown of rules.
+      </h3>
+      <i class="d-md-block d-none">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Non laudantium aspernatur quod, possimus culpa temporibus perferendis sed sapiente excepturi?
+        Sapiente, nemo quae? Ex, dolores rem. Explicabo cumque provident magni? Obcaecati, dolor. Cupiditate
+        accusamus soluta provident nobis, deserunt in neque dignissimos error assumenda, consequuntur recusandae,
+        totam laborum. Inventore accusantium sint, reprehenderit et exercitationem dolorem tempore esse praesentium,
+        cumque culpa saepe eaque harum ad. Dolorem perspiciatis, praesentium amet doloribus dolore officia ipsam
+        nemo harum ipsum distinctio voluptas.</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RulesComponent'
+
+}
+</script>
+
+<style scoped>
+h2 {
+   font-size: 2.5rem;
+ }
+ p {
+   font-size: 2rem;
+ }
+ i {
+   font-size: 1.5rem;
+ }
+ button {
+   font-size: 1.25rem;
+ }
+ img{
+   height: 30rem;
+   width: 25;
+ }
+</style>
