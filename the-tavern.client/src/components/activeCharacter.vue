deba9d3973c47d714b3bf87c12b84fef73e5f816
Kevin Powell <55499063+kpohiostyle@users.noreply.github.com>
2021-05-14 16:29:24 -0600

    account page updates

--- the-tavern.client/src/components/activeCharacter.vue
+++ the-tavern.client/src/components/activeCharacter.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="row my-5">
-    <div class="col-md-4  text-center">
-      <img src="//placehold.it/250x250" alt="">
-    </div>
-    <div class="col-md-6 shadow">
-      <h5><u>Name: {{ state.character.name }}</u></h5>
-      <p>Job: {{ state.character.job }}</p>
-      <p>Race: {{ state.character.race }}</p>
-      <p>Speed: {{ state.character.speed }}</p>
-      <p>Hit Points: {{ state.character.health }}</p>
-    </div>
-  </div>
-  <div class="row pr-5 mr-5">
-    <div class="about col-12">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut labore corporis velit
-        id aliquid debitis excepturi odio earum. Reprehenderit excepturi quibusdam laborum
-        amet, vero numquam molestias dignissimos porro nulla? Consequuntur.
-        Expedita similique aut repellat, esse distinctio magnam non recusandae, error,
-        laudantium ut iure quis accusantium aspernatur quam mollitia accusamus tempore.
-        Sit ullam, sapiente rerum qui recusandae autem cumque ex voluptate!
-      </p>
-    </div>
-  </div>
   <div>
-    <StatsComponent />
-  </div>
-  <div class="row">
+    <div class="row my-5">
+      <div class="col-md-4 text-center">
+        <img src="//placehold.it/250x250" class="w-100" alt="">
+      </div>
+      <div class="col-md-6 shadow bg-light">
+        <h5>Name: <span class="editName" @click.stop="" spellcheck="false" contenteditable="true" @blur="editName"> {{ state.character.name || 'Add Name' }}</span></h5>
+        <p>Job: {{ state.character.job }}</p>
+        <p>Race: {{ state.character.race }}</p>
+        <p>Speed: {{ state.character.speed }}</p>
+        <p>Hit Points: {{ state.character.health }}</p>
+      </div>
+    </div>
+    <div class="row pr-md-5 mr-md-5 px-2">
+      <div class="about col-12">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut labore corporis velit
+          id aliquid debitis excepturi odio earum. Reprehenderit excepturi quibusdam laborum
+          amet, vero numquam molestias dignissimos porro nulla? Consequuntur.
+          Expedita similique aut repellat, esse distinctio magnam non recusandae, error,
+          laudantium ut iure quis accusantium aspernatur quam mollitia accusamus tempore.
+          Sit ullam, sapiente rerum qui recusandae autem cumque ex voluptate!
+        </p>
+      </div>
+    </div>
+    <div class="row my-3">
+      <StatsComponent />
+    </div>
+    <div class="row">
     <!-- <EquipmentComponent /> -->
+    </div>
   </div>
 </template>
 <script>
@@ -35,25 +37,37 @@ import { computed, reactive } from 'vue'
 // import { charactersService } from '../services/CharactersService'
 // import { useRoute } from 'vue-router'
 import { AppState } from '../AppState'
+import { charactersService } from '../services/CharactersService'
+import Notification from '../utils/Notification'
 export default {
   name: 'ActiveCharacter',
   setup() {
     const state = reactive({
-      character: computed(() => AppState.activeCharacter)
+      character: computed(() => AppState.activeCharacter),
+      showActive: false
     })
-    // onMounted(async() => {
-    //   try {
-    //     await charactersService.setActiveCharacter(route.params.id)
-    //   } catch (error) {
-    //     Notification.toast('Error:' + error, 'error')
-    //   }
-    // })
     return {
-      state
+      state,
+      async editName(event) {
+        try {
+          state.character.name = event.target.innerText
+          await charactersService.editName(state.character)
+        } catch (error) {
+          Notification.toast('Error: ' + error, 'error')
+        }
+      }
     }
   }
 }
 </script>
 <style scoped>
-
+.editName{
+  min-width: 2rem;
+}
+.editName:hover{
+  border: 1px dashed var(--primary);
+}
+img{
+  max-width: 40vh;
+}
 </style>
--- the-tavern.client/src/pages/AboutPage.vue
+++ the-tavern.client/src/pages/AboutPage.vue
@@ -25,39 +25,41 @@
       <div class="col-md-8 col-12 p-md-5" v-if="state.show=='rec'">
         <RecComponent />
       </div>
-      <div class="TavernKeeper col-4 d-md-block d-none bg-primary p-5 pt-5">
+      <div class="TavernKeeper col-4 d-md-block d-none bg-primary p-xl-5">
         <div class="shadow bg-light text-center m-3 p-5">
           <div>
-            <img src="https://imgur.com/a/Cy90ukc" alt="Friendly Tavern Keeper">
+            <img src="https://ibb.co/7jQxfhV" alt="Friendly Tavern Keeper">
           </div>
           <i>You must be new in town. Let me know if you have any questions!</i>
           <div class="row justify-content-center">
-            <div class="col-6 text-center my-2">
+            <div class="col-12 text-center my-2">
               <button type="button" class="btn btn-primary w-100" title="classes" @click="toggle('classes')">
                 Classes
               </button>
             </div>
-            <div class="col-6 text-center my-2">
+            <div class="col-12 text-center mb-2">
               <button type="button" class="btn btn-primary w-100" title="DnD races" @click="toggle('races')">
-                Races
+                <span class="w-100">
+                  Races
+                </span>
               </button>
             </div>
-            <div class="col-6 text-center my-2">
+            <div class="col-12 text-center mb-2">
               <button type="button" class="btn btn-primary w-100" title="backgrounds" @click="toggle('background')">
                 Backgrounds
               </button>
             </div>
-            <div class="col-6 text-center my-2">
+            <div class="col-12 text-center mb-2">
               <button type="button" class="btn btn-primary w-100" title="equipment" @click="toggle('equipment')">
                 Equipment
               </button>
             </div>
-            <div class="col-6 text-center my-2">
+            <div class="col-12 text-center mb-2">
               <button type="button" class="btn btn-primary w-100" title="basic rules" @click="toggle('rules')">
                 Basic Rules
               </button>
             </div>
-            <div class="col-6 text-center my-2">
+            <div class="col-12 text-center mb-2">
               <button type="button" class="btn btn-primary w-100" title="terminology and definitions" @click="toggle('term')">
                 Terminology
               </button>
@@ -76,8 +78,7 @@
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
-import { AppState } from '../AppState'
+import { reactive } from 'vue'
 export default {
   name: 'AboutPage',
   props: {
@@ -88,9 +89,7 @@ export default {
   },
   setup() {
     const state = reactive({
-      show: 'basics',
-      characters: computed(() => AppState.characters),
-      activeCharacter: computed(() => AppState.activeCharacter)
+      show: 'basics'
     })
 
     return {
