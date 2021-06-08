fa0e2be071b9a8c9c0302043216e4705f3ff5887
Kevin Powell <55499063+kpohiostyle@users.noreply.github.com>
2021-06-08 09:54:28 -0600

    one last change

--- the-tavern.client/vue.config.js
+++ the-tavern.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: 'docs',
+  outputDir: '../the-tavern.server/client',
   productionSourceMap: false
 }
