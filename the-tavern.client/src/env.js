a7b0bff7cf7df0d7e9735b880e01d6f04380a000
Kevin Powell <55499063+kpohiostyle@users.noreply.github.com>
2021-06-08 09:52:39 -0600

    one change

--- the-tavern.client/src/env.js
+++ the-tavern.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'kevincodeworks.us.auth0.com'
-export const audience = 'http://kanban'
-export const clientId = 'MV7tjf8CPDJceBrMRZ0aUlr920C2cMVq'
+export const audience = 'https://theTavern'
+export const clientId = '1G72hInDNsZjtGVi24vTcaWrgWCWqXx7'
